28d9fda215a724b779178fb9efe2ef174a670d1d
Manneytan <manlitan008@gmail.com>
2026-07-21 16:41:51 -0400

    Update 01_executive_kpi.sql

--- sql/01_executive_kpi.sql
+++ sql/01_executive_kpi.sql
@@ -1,36 +1,20 @@
+-- KPI 1 Total GMV
+SELECT ROUND(SUM(price),2) AS total_gmv
+FROM order_items;
+
+-- KPI 2 Total Orders
+SELECT COUNT(DISTINCT order_id) AS total_orders
+FROM orders;
+
+-- KPI 3 Active Sellers
+SELECT COUNT(DISTINCT seller_id) AS active_sellers
+FROM sellers;
+
+-- KPI 4 Active Customers
+SELECT COUNT(DISTINCT customer_id) AS active_customers
+FROM customers;
+
+-- KPI 5 Average Order Value (AOV)
 SELECT
-
-    ROUND(SUM(oi.price), 2) AS total_gmv,
-
-    COUNT(DISTINCT o.order_id) AS total_orders,
-
-    COUNT(DISTINCT oi.seller_id) AS active_sellers,
-
-    COUNT(DISTINCT c.customer_unique_id) AS active_customers,
-
-    COUNT(DISTINCT oi.product_id) AS total_products,
-
-    ROUND(
-        SUM(oi.price) / COUNT(DISTINCT o.order_id),
-        2
-    ) AS avg_order_value,
-
-    ROUND(
-        COUNT(*) * 1.0 / COUNT(DISTINCT o.order_id),
-        2
-    ) AS avg_items_per_order,
-
-    ROUND(
-        AVG(oi.freight_value),
-        2
-    ) AS avg_shipping_fee
-
-FROM orders o
-
-JOIN order_items oi
-ON o.order_id = oi.order_id
-
-JOIN customers c
-ON o.customer_id = c.customer_id
-
-WHERE o.order_status = 'delivered';
+ROUND(SUM(price)/COUNT(DISTINCT order_id),2) AS aov
+FROM order_items;

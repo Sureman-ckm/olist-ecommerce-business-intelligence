b3fe131bc5a56dc840dc58d9f243dc17c435e5a4
Manneytan <manlitan008@gmail.com>
2026-07-21 16:30:30 -0400

    Update 01_executive_kpi.sql

--- sql/01_executive_kpi.sql
+++ sql/01_executive_kpi.sql
@@ -1,1 +1,26 @@
+SELECT COUNT(*) AS total_orders
+FROM orders;
+
+SELECT
+ROUND(SUM(price),2) AS total_gmv
+FROM order_items;
+
+SELECT
+ROUND(SUM(price)/COUNT(DISTINCT order_id),2) AS average_order_value
+FROM order_items;
+
+SELECT COUNT(DISTINCT seller_id)
+FROM sellers;
+
+SELECT COUNT(*)
+FROM products;
+
+SELECT
+seller_id,
+SUM(price) AS gmv
+FROM order_items
+GROUP BY seller_id
+ORDER BY gmv DESC
+LIMIT 10;
+
 

f7c81ad52c93daa996db79744ce6ae47b0ef0d54
Manneytan <manlitan008@gmail.com>
2026-07-21 16:42:20 -0400

    Update 02_category_analysis.sql

--- sql/02_category_analysis.sql
+++ sql/02_category_analysis.sql
@@ -1,1 +1,18 @@
-
+SELECT
+    COALESCE(t.product_category_name_english, p.product_category_name) AS category,
+    ROUND(SUM(oi.price),2) AS gmv,
+    COUNT(DISTINCT oi.order_id) AS orders,
+    ROUND(AVG(oi.price),2) AS avg_price,
+    ROUND(
+        SUM(oi.price) * 100.0 /
+        SUM(SUM(oi.price)) OVER (),
+        2
+    ) AS gmv_share
+FROM order_items oi
+JOIN products p
+    ON oi.product_id = p.product_id
+LEFT JOIN product_category_translation t
+    ON p.product_category_name = t.product_category_name
+GROUP BY
+    COALESCE(t.product_category_name_english, p.product_category_name)
+ORDER BY gmv DESC;

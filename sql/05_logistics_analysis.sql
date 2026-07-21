2d8c9a23093936d8990da82dc21326943e6d78de
Manneytan <manlitan008@gmail.com>
2026-07-21 16:43:27 -0400

    Update 05_logistics_analysis.sql

--- sql/05_logistics_analysis.sql
+++ sql/05_logistics_analysis.sql
@@ -1,1 +1,50 @@
+SELECT
+    o.order_status,
 
+    ROUND(
+        AVG(
+            EXTRACT(
+                DAY FROM (
+                    o.order_delivered_customer_date -
+                    o.order_purchase_timestamp
+                )
+            )
+        ),
+        2
+    ) AS delivery_days,
+
+    ROUND(
+        AVG(
+            EXTRACT(
+                DAY FROM (
+                    o.order_estimated_delivery_date -
+                    o.order_delivered_customer_date
+                )
+            )
+        ),
+        2
+    ) AS delay_days,
+
+    ROUND(AVG(r.review_score),2) AS avg_review_score,
+
+    ROUND(AVG(oi.freight_value),2) AS avg_freight,
+
+    ROUND(AVG(oi.price),2) AS avg_price,
+
+    COUNT(DISTINCT o.order_id) AS orders
+
+FROM orders o
+
+JOIN order_items oi
+ON o.order_id = oi.order_id
+
+LEFT JOIN order_reviews r
+ON o.order_id = r.order_id
+
+WHERE o.order_delivered_customer_date IS NOT NULL
+
+GROUP BY
+    o.order_status
+
+ORDER BY
+    orders DESC;
